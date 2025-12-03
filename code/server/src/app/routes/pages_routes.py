e3ba1d808f9b24bc8e1a2b2c67e1c4e702cbca97
Matheus Figueiredo <108003682+mfigueireddo@users.noreply.github.com>
2025-12-03 09:43:46 -0300

    Rota de produtos

--- code/server/src/app/routes/pages_routes.py
+++ code/server/src/app/routes/pages_routes.py
@@ -5,9 +5,13 @@ frontend_tag = Tag(name="Frontend", description="Rotas de páginas HTML")
 
 def register_pages_routes(app):
     @app.get("/", tags=[frontend_tag])
-    def produtos_page():
+    def produtos_homepage():
         return render_template("products.html")
     
+    @app.get("/page/produtos", tags=[frontend_tag])
+    def produtos_page():
+        return render_template("products.html")
+
     @app.get("/page/vendas", tags=[frontend_tag])
     def vendas_page():
         return render_template("sales.html")
